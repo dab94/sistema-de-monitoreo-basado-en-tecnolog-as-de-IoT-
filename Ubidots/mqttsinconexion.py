2cd5a4b2ddde24b5be244ca131be532f0e88cda3
dab94 <dab-94@hotmail.com>
2020-12-14 01:03:00 -0500

    Update mqttsinconexion.py

--- Ubidots/mqttsinconexion.py
+++ Ubidots/mqttsinconexion.py
@@ -1,18 +1,17 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 
-hola='NO'
+
 def on_connect(client, userdata, flags, rc):
     print ("Connected with result code"+str(rc))
     client.subscribe("#",0)
 
     
 def on_message(client,userdata,msg):
-    global hola
+ 
     print (msg.topic+" "+str(msg.payload))
-    publish.single(msg.topic,msg.payload,0,hostname='things.ubidots.com',auth = {'username':"n31rZtaWi1j5umLjF906IJeH9kcff4"})
-    #hola=msg.payload
-    #print('Aqui', hola)
+    publish.single(msg.topic,msg.payload,0,hostname='things.ubidots.com',auth = {'username':""})
+    
     
 client=mqtt.Client()
 client.on_connect=on_connect
